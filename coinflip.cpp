8d6cb115e4a7b2eb281b0d3f3ebdfb522ead5ddc
Changzy <63215608+Cool-CoCoder@users.noreply.github.com>
2023-01-27 15:03:43 +0800

    Update coinflip.cpp

--- coinflip.cpp
+++ coinflip.cpp
@@ -3,10 +3,9 @@
 //
 #include "iostream"
 #include "stdlib.h"
-#include "time.h"
-
-
 using namespace std;
+
+int random_memory = -1;
 
 class Coin {
     int side; // 1 is positive, 0 is negative
@@ -39,12 +38,12 @@ public:
 };
 
 
-int Coin::seed = ::time(0);
+int Coin::seed = (long)&random_memory;
 
 int main() {
     while (true) {
         int n;
-        cout << "How many coins do you want to toss:";
+        cout << "How many coins do you want to toss:(do not enter a number over 2 million)" << endl;
         cin >> n;
         Coin coin[n];
         int count_pos = 0, count_neg = 0;
@@ -56,13 +55,16 @@ int main() {
                 cout << 1 << "\t";
                 count_pos++;
             }
+            if((i+1)%9 == 0){
+                cout <<endl;
+            }
         }
         cout << endl;
         cout << "The total positive number is:" << count_pos << endl;
         cout << "The total negative number is:" << count_neg << endl;
         float prob_pos = (float) count_pos / n;
         cout << "The probability of positive is:" << prob_pos << endl;
-        cout << "Do you want to paly again?Y/N" << endl;
+        cout << "Do you want to paly again?(Y/N)" << endl;
         char respond;
         cin >> respond;
         if (respond == 'N') break;
