5a56739f0db6f45b894effc0da04328aeabc44ae
Changzy <63215608+Cool-CoCoder@users.noreply.github.com>
2023-01-28 15:19:19 +0800

    I added a new game--dice rolling into it.

--- coinflip.cpp
+++ coinflip.cpp
@@ -7,66 +7,136 @@ using namespace std;
 
 int random_memory = -1;
 
-class Coin {
-    int side; // 1 represent head, 0 represent tail
+class Random_Event{
 public:
+    int side;
     static int seed; // the seed need to be static
+    virtual void toss() = 0; // My experiment has shown that 0~9 is uniformly distributed by the operation rand()%10
+    virtual void play() = 0;
 
-    int getSide() const {
-        return side;
-    }
+};
 
-    void setSide(int side) {
-        Coin::side = side;
-    }
+int Random_Event::seed = (long)&random_memory;
 
-    Coin(int side) { this->side = side; }
 
+// 1 represent head, 0 represent tail
+class Coin : public Random_Event{
+public:
     Coin() {
-        setSide(toss());
+        toss();
     }
 
-    int toss() {
+    void play() override{
+        while (true) {
+            int n;
+            cout << "How many coins do you want to toss:(no more than INT_MAX)" << endl;
+            cout << ">";
+            cin >> n;
+            int count_pos = 0, count_neg = 0;
+            for (int i = 0; i <= n; ++i) {
+                Coin *coin = new Coin();
+                if(coin->side == 1){
+                    count_pos ++;
+                } else {
+                    count_neg ++;
+                }
+                delete coin;
+
+            }
+            cout << endl;
+            cout << "The number of times when the coin is head is:" << count_pos << endl;
+            cout << "The number of times when the coin is tail is:" << count_neg << endl;
+            float prob_pos = (float) count_pos / n;
+            cout << "The ratio of head is:" << prob_pos << endl;
+            cout << "Do you want to play again?(Y/N)" << endl;
+            char respond;
+            cin >> respond;
+            if (respond == 'N') break;
+        }
+    }
+
+private:
+
+    void toss() override {
         ::srand(seed);
         int temp = rand();
         seed = temp;
         if (temp % 2 == 1) {
-            return 1;
+           side = 1;
+        } else {
+            side = 0;
         }
-        return 0;
+
+    }
+
+};
+
+class dice : public Random_Event{
+public:
+    dice(){
+        toss();
+    }
+
+    void play() override{
+        int count[6] = {0};
+        int n;
+        while(true){
+            cout << "How many dices do you want to roll?(no more than INT_MAX)" << endl;
+            cout << ">";
+            cin >> n;
+            for (int i = 0; i < n; ++i) {
+                dice *d = new dice();
+                count[d->side] ++;
+                delete d;
+            }
+            cout << endl;
+            for (int i = 0; i < 6; ++i) {
+                cout << "The occurrence time of "<< i <<" is "<<count[i];
+                cout << ". The ratio is: " << (float) count[i]/(float )n << endl;
+            }
+            cout << "Do you want to play again?(Y/N)" << endl;
+            char respond;
+            cin >> respond;
+            if (respond == 'N') break;
+        }
+    }
+private:
+    // dice has six sides
+    void toss() override{
+        // 如何让6个不同的事件均匀出现
+        ::srand(seed);
+        int temp = rand();
+        seed = temp;
+        while(temp%10 >5){ // we only want 0~5 uniformly come out
+            temp = ::rand();
+            seed = temp;
+        }
+        side = temp%10;
     }
 };
 
-
-int Coin::seed = (long)&random_memory;
-
 int main() {
-    while (true) {
-        int n;
-        cout << "How many coins do you want to toss:(no more than INT_MAX)" << endl;
-        cin >> n;
-        int count_pos = 0, count_neg = 0;
-        for (int i = 0; i <= n; ++i) {
-            Coin *coin = new Coin();
-            if(coin->getSide() == 1){
-                count_pos ++;
-            } else {
-                count_neg ++;
+    while(true){
+        int type;
+        cout << "Which game do you want to play?(give the number)" << endl;
+        cout << "1.Tossing coins." << endl;
+        cout << "2.Rolling dices." << endl;
+        cout << ">";
+        cin >> type;
+        switch (type) {
+            case 1:{
+                Coin coin;
+                coin.play();
+                break;
             }
-            delete coin;
-
+            case 2: {
+                dice dice;
+                dice.play();
+                break;
+            }
+            default:
+                cout <<"Please enter a right number!" << endl;
         }
-
-        cout << endl;
-        cout << "The total head times is:" << count_pos << endl;
-        cout << "The total tail times is:" << count_neg << endl;
-        float prob_pos = (float) count_pos / n;
-        cout << "The probability of head is:" << prob_pos << endl;
-        cout << "Do you want to paly again?(Y/N)" << endl;
-        char respond;
-        cin >> respond;
-        if (respond == 'N') break;
     }
-
 
 }
